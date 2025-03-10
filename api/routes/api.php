b5fff04981263a192c144fd0608680d4e2f7f355
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-10 10:42:52 +0000

    crétion du controleur

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Product_vetementsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('products', Product_vetementsController::class);
